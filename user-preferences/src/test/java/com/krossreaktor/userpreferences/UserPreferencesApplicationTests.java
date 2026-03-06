b0b46843658292a801efdee3045f228164ac1244
brunoduarte <brunoduarte@icligo.com>
2026-03-06 08:57:26 +0000

    test

---
+++ user-preferences/src/test/java/com/krossreaktor/userpreferences/UserPreferencesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krossreaktor.userpreferences;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPreferencesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
